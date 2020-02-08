d003c4d9a7465ab0d5df9e865ddffb2c11084a0f
Maxel <gigaabait@yandex.ru>
2020-02-08 21:09:01 +0400

    Проба

--- gulpfile.js
+++ gulpfile.js
@@ -90,7 +90,7 @@ gulp.task("images", function() {
 
 gulp.task("webp", function() {
   return gulp
-    .src("img/*.{png,jpg}")
+    .src("source/img/*.{png,jpg}")
     .pipe(webp({ quality: 90 }))
     .pipe(gulp.dest("build/img"));
 });
